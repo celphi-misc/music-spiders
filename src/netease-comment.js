09b698adb5891e4fe98c4fa259a6b890dc77cf4e
celestialphineas <hanghang0713@hotmail.com>
2018-11-21 21:48:54 +0800

    Proxies!

--- src/netease-comment.js
+++ src/netease-comment.js
@@ -1,32 +1,72 @@
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
+const proxyList = require('./evil/proxyList');
 
+// ==== Configurations ====
+// Step: Create new file by this number of songs
+var step = 10;
+var maxTries = 50;
 // NetEase music API base URL
 axios.defaults.baseURL = 'http://localhost:3000';
 
+
+function getRandomProxy() {
+  let index = Math.floor(Math.random() * proxyList.length);
+  return 'http://' + proxyList[index];
+}
+
+// Meta
 var meta = fs.readFileSync(path.join(__dirname, '../data/meta/meta-verified.json'), 'utf8');
 meta = JSON.parse(meta);
+
+// Clear log
+const logFilename = path.join(__dirname, '../log/netease-comments.log');
+fs.writeFileSync(logFilename, '');
+function writeLog(str) { fs.appendFileSync(logFilename, str + '\n'); }
+
 
 // Total: 645
 // console.log(Object.keys(meta).length);
 
 async function getComments(id) {
   var comments = [];
-  let res = await axios.get('/comment/music', { params: { id, limit: 20, offset: 0 } });
+  var proxy;
+  for(var i = 0; i < maxTries; i++) {
+    try {
+      proxy = getRandomProxy(); 
+      var res = await axios.get('/comment/music', { params: { id, limit: 20, offset: 0, proxy } });
+      break;
+    } catch(err) {
+      writeLog(`${new Date()}\tCannot get NE id ${id} from ${proxy}`);
+    }
+  }
+  if(i == maxTries) {
+    writeLog(`${new Date()}\tFAILURE: Max tries exceeded for NE id ${id}`);
+    return [];
+  }
   let data = res.data;
   let count = data.total;
   console.log(`Song ${id}: ${count} comments`);
   comments = data.comments.splice();
-  await (async function () {
-    for(let i = 1; i * 20 < count; i ++) {
-      await axios.get('/comment/music', { params: {
-        id, limit: 20, offset: i } })
-          .then(res => {
-            res.data.comments.forEach(comment => comments.push(comment));
-          }).catch(err => console.err);
+
+  for(let i = 1; i * 20 < count; i ++) {
+    for(var j = 0; j < maxTries; j++) {
+      try {
+        proxy = getRandomProxy();
+        let res = await axios.get('/comment/music', { params: {
+          id, limit: 20, offset: i, proxy } });
+        if(res.data.comments) {
+          res.data.comments.forEach(comment => comments.push(comment));
+          break;
+        }
+        else throw Error();
+      } catch(err) {
+        writeLog(`${new Date()}\tCannot get NE id ${id} from ${proxy}`);
+      }
     }
-  })();
+    if(j == maxTries) writeLog(`${new Date()}\tFAILURE: Max tries exceeded for NE id ${id}`);
+  }
   return comments;
 }
 
@@ -39,8 +79,10 @@ async function getComments(id) {
     console.log(`${Math.ceil(Number.parseInt(id)/10)}%`);
     buffer[id] = comments;
     count++;
-    if(count >= 20) {
-      fs.writeFileSync(path.join(__dirname, `../data/comments/netease/${count}.json`), JSON.stringify(buffer));
+    if(count >= step) {
+      if(step === 1) var outFilename = `id-${id}.json`;
+      else var outFilename = `${count}.json`;
+      fs.writeFileSync(path.join(__dirname, `../data/comments/netease/${outFilename}`), JSON.stringify(buffer));
       count = 0;
       buffer = {};
       await new Promise(resolve => setTimeout(resolve, 5000));
